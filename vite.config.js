334448037f2b894f039eead3dda1935fb6a950bf
fahrizal <muchamad.fachrizal12@gmail.com>
2025-07-08 08:48:53 +0700

    Add Host For Local And Dev

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,9 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         hmr: {
+            // host untuk server
             host: '10.14.39.147',
+            // host untuk local
             // host: '127.0.0.1',
             protocol: 'ws'
         },
