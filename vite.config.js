9e8a2f2eaedafbdcd09718217b47df1186d64a26
fahrizal <muchamad.fachrizal12@gmail.com>
2025-07-10 11:55:58 +0700

    style(views): adjust container width to full width for better layout

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,9 @@ export default defineConfig({
         host: '0.0.0.0',
         hmr: {
             // host untuk server
-            host: '10.14.39.147',
+            // host: '10.14.39.147',
             // host untuk local
-            // host: '127.0.0.1',
+            host: '127.0.0.1',
             protocol: 'ws'
         },
         watch: {
